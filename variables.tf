cfe4c7450164a1a642edb1ac275ed25b79a08bfb
John Farnell <jbfarnell@gmail.com>
2020-09-16 11:18:24 -0400

    create multi ws-clean

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "workspace_names" {
   type = list(string)
-  #default = ["AWS-Networking-SBX", "AWS-Networking-DEV", "AWS-Networking-TST", "AWS-Networking-PRD"]
+  default = ["AWS-Networking-SBX", "AWS-Networking-DEV", "AWS-Networking-TST", "AWS-Networking-PRD"]
 }
 
 /* variable "api_token" {
