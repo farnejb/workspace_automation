d360be77e7e60339039190ec7ffe8bb133671e30
John Farnell <jbfarnell@gmail.com>
2020-09-16 11:09:45 -0400

    create multi ws with tfvars

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "workspace_names" {
+  type = list(string)
+  default = ["AWS-Networking-SBX", "AWS-Networking-DEV", "AWS-Networking-TST", "AWS-Networking-PRD"]
+}
+
 /* variable "api_token" {
   type = string
 } */
