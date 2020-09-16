7bb8f72c4b76ccffb679de986f873272f80ca81c
John Farnell <jbfarnell@gmail.com>
2020-09-16 10:55:09 -0400

    create multi ws

--- main.tf
+++ main.tf
@@ -1,9 +1,26 @@
 provider "tfe" {
-  hostname = "firefly.tfe.rocks"
+  hostname = ""
 }
 
-resource "tfe_workspace" "test" {
+resource "tfe_workspace" "example" {
+  count        = length(var.workspace_names)
+  name         = var.workspace_names[count.index]
+  organization = "my-org"
+}
+
+/* resource "tfe_workspace" "test" {
   name         = "my_automated_workspace"
   organization = "my-org"
 }
+
+resource "tfe_workspace" "test2" {
+  name = "second_workspace"
+  organization = "test-org"
+}
+
+resource "tfe_team" "first_team" {
+  name         = "first-team"
+  organization = "test-org"
+} */
+
 
